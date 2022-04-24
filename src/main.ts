8b1a72a65d06abc6fa5566c7c78f9efa3ca6f688
MIZUSHIN <taurus.nebula@gmail.com>
2022-04-24 22:32:29 +0900

    修正

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
-import { createApp, VueElement } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const app = createApp(App)
-
-library.add(fas,fab)
-
-app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
