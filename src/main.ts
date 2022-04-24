3eb4498bdcc6995c4c7dad75859252f9a19f3cbb
MIZUSHIN <taurus.nebula@gmail.com>
2022-04-24 18:16:32 +0900

    Font Awesomeの追加

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+library.add(faCoffee)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
