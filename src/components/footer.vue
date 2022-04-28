11392d8133140b23e60312755b10ae0493e9660b
MIZUSHIN <taurus.nebula@gmail.com>
2022-04-28 23:02:53 +0900

    footerコンポーネント追加

---
+++ src/components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <nav>
+      <a><router-link to="/">Home</router-link></a> |
+      <a><router-link to="/about">About</router-link></a> |
+      <a><router-link to="/dev">Dev</router-link></a> |
+      <a><router-link to="/posts">Posts</router-link></a>
+    </nav>
+    <p>© 2022 MIZUSHIN. All rights reserved.</p>
+  </footer>
+</template>
