0926d36b47adf5b1677d34805ec1ed387a9d66a8
MIZUSHIN <taurus.nebula@gmail.com>
2022-05-27 23:29:21 +0900

    devページの改修

--- src/views/Dev.vue
+++ src/views/Dev.vue
@@ -2,6 +2,7 @@
   <div>
     <h2>PORTFOLIO(2022)</h2>
     <a herf="mizushin.net">リンク</a>
+    <img src="../assets/portfolio2022.png" href="mizushin.net">
   </div>
 </template>
 
@@ -13,5 +14,10 @@
   transform: translate(-50%, -50%);
   top: 50%;
   left: 50%;
+}
+
+img {
+  border: solid;
+  width: 15vw;
 }
 </style>
