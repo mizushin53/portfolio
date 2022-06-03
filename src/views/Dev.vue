ac4b6724f5fefc7a2c2ef73f711988185b9e5cae
MIZUSHIN <taurus.nebula@gmail.com>
2022-06-03 23:32:56 +0900

    devページの改修

--- src/views/Dev.vue
+++ src/views/Dev.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <h3>PORTFOLIO(2022)</h3>
-    <img src="../assets/portfolio2022.webp" href="mizushin.net">
+    <img src="../assets/portfolio2022.webp">
   </div>
 </template>
 
 <style>
 
 h3 {
-  display: flex;
+  font-size: clamp(12px, 2vw, 18px);
+  margin: 0px 10px 0px 10px;
+  text-align: flex;
 }
 
 img {
   border: solid;
   width: 15vw;
+  display: flex;
 }
 </style>
