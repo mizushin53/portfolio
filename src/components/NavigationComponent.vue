467fcf95759c98868fede2045ec12993a9a73ccf
MIZUSHIN <taurus.nebula@gmail.com>
2022-06-11 22:35:42 +0900

    Navigationコンポーネントの追加

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -1,9 +1,9 @@
 <template>
   <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Dev</li>
-      <li>Posts</li>
+    <li><router-link to="/">Home</router-link></li>
+    <li><router-link to="/about">About</router-link></li>
+    <li><router-link to="/dev">Dev</router-link></li>
+    <li><router-link to="/posts">Posts</router-link></li>
   </ul>
   <ul>
     <li>Twitter</li>
